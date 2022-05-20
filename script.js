a3ac6728ccdf0f4256865645e876e7a4c35a944c
Kofi <sambonful@hotmail.com>
2022-05-21 00:31:50 +0100

    Add live weather updates

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+
+
 const div = document.createElement("div");
 const box = document.createElement("div");
 const toDoDiv = document.createElement("div");
@@ -81,7 +84,7 @@ checkHours()
 
 const units = "metric"
 const key = "8a17353366f90cc63c75f2704f902965"
-const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=${units}&appid=${key}`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=London&${units}&appid=${key}`
 
 const getWeather = async () => {
     const response = await fetch(url)
